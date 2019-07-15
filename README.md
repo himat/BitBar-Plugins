0500ade391d3cf849d1b88bcdc5c0e52c6079547
Hima <himat15@yahoo.com>
2019-07-15 19:17:01 -0400

    Created BitBar repo and timezones plugin

---
+++ README.md
@@ -0,0 +1,11 @@
+# My BitBar plugins
+
+- Timezones
+  - Shows times of various world locations upon clicking on the watch emoji in the Menu Bar
+![timezones screenshot](./images/timezones.png)
+
+
+
+To change update frequency, rename the `n` in a file here in the format `name.n.sh` to a time of the form `<number><s/m/h>` (seconds, minutes, hours).
+
+[BitBar](https://github.com/matryer/bitbar)
---
+++ timezones.1m.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Displays time zones in the drop down.
+#
+# <bitbar.title>Timezones</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Hima Tammineedi</bitbar.author>
+# <bitbar.author.github>himat</bitbar.author.github>
+# <bitbar.desc>Display with various timezones in the drop-down menu</bitbar.desc>
+# <bitbar.image>https://cloud.githubusercontent.com. TODO*********</bitbar.image>
+
+echo ":watch:" # Menu bar will show a watch emoji
+echo '---' # Lines below will appear in dropdown
+echo -n "New York " ; TZ="America/New_York"; date +'%-l:%M %p';
+echo -n "Beijing "; export TZ="Asia/Shanghai"; date +'%-l:%M %p';
+echo -n "London "; export TZ="Europe/London"; date +'%-l:%M %p';
+echo -n "Delhi "; export TZ="Asia/Kolkata"; date +'%-l:%M %p';
